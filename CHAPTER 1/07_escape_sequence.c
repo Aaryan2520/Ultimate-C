7c98a89905d4f1c260fe5bd33c0483114084e975
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-07-16 17:15:50 +0530

    Create 07_escape_sequence.c

---
+++ CHAPTER 1/07_escape_sequence.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch = 'a';
+    ch = 'c';
+    ch = '\n'; // represent new line
+    printf("Hey I am good \n nice \n\n\n");
+
+    // Inserts a newline character into the string.
+    printf("Hello\nWorld \n");
+
+
+    // Inserts a tab space.
+    printf("Hello\tWorld \n");
+
+
+    // Inserts a backslash character.
+    printf("C:\\Program Files \n");
+
+
+    // Inserts a double quote character.
+    printf("She said, \"Hello\" \n");
+
+
+    // Inserts a single quote character.
+    printf("It\'s a nice day \n");
+
+
+    // Produces an audible or visible alert.
+    printf("\a \n"); 
+    // might produce a beep sound (depending on the system).
+
+    // Moves the cursor one position back.
+    printf("Hello\b\bWorld \n");
+
+
+    // Moves the cursor to the beginning of the line.
+    printf("Hello\rWorld \n");
+
+
+    // Advances the cursor to the next page (not commonly used in modern systems).
+    printf("Hello\fWorld \n");
+
+    // Moves the cursor to the next vertical tab stop.
+    printf("Hello\vWorld \n");
+
+
+    return 0;
+}
