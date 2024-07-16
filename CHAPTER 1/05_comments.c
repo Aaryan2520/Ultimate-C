7a0c45e081b734dd5bf3ec5d93a39d56bd28b4b4
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-07-16 17:40:56 +0530

    Update 05_comments.c

--- CHAPTER 1/05_comments.c
+++ CHAPTER 1/05_comments.c
@@ -6,7 +6,8 @@ Date: 13 July 2024
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
     // I am writing this judt for. This line doesnt do anything.
     printf("Hello Worls");
     return 0; // This is return statement
