e05765be623f4c408e39348590cfb92450abd45b
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-07-16 15:39:43 +0530

    Create 02_variable_rules.c

---
+++ CHAPTER 1/02_variable_rules.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a; // Allowed
+    int b; // Allowed
+    int c; // Allowed
+    int cab; // Allowed
+    int harry; // Allowed
+    int harry is good; //Not Allowed
+    int harry$good;  //Not  Allowed
+    int harry#good; //Not Allowed
+    int harry(())good;  //Not Allowed
+    int harry_good; // Allowed
+
+
+    return 0;
+}
