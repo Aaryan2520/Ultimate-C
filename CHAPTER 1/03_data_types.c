ee2e741133eee40f940fdfd0815e178e7fcd7d40
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-07-16 16:21:30 +0530

    Update 03_data_types.c

--- CHAPTER 1/03_data_types.c
+++ CHAPTER 1/03_data_types.c
@@ -14,5 +14,10 @@ int main()
     char c;
     c = 'a';
 
+    printf("The value of a is %d \n",a);
+    printf("The value of b is %f \n",b);
+    printf("The value of c is %c \n",c);
+
+
     return 0;
 }
