e8fe939e7280474391b7b12b1e860123b27a66b9
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-07-16 16:11:03 +0530

    Create 05_comments.c

---
+++ CHAPTER 1/05_comments.c
@@ -0,0 +1,13 @@
+/*
+Program: 05_comments.c
+Author: Aryan
+Date: 13 July 2024
+*/
+
+#include <stdio.h>
+
+int main(){
+    // I am writing this judt for. This line doesnt do anything.
+    printf("Hello Worls");
+    return 0; // This is return statement
+}
