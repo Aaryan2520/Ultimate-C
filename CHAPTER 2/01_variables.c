38c7aa9507cf3c7cc26ac012f42ed4718ff7c9d3
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-07-27 21:09:03 +0530

    Create 01_variables.c

---
+++ CHAPTER 2/01_variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10; // declarenand initialize 'i' with 10
+    int j = i;
+    int a = 2, b = 3, c = 4, d = 5;
+    // %d is called the format specifier
+    // %d is for int, %f is for float, %c is gor char
+
+    printf("The value of i is %d and the value of j is %d \n", i , j ) ;
+    printf("The value of a is %d and the value of b is %d \n", a , b ) ;
+    printf("The value of c is %d and the value of d is %d \n", c , d ) ;
+
+    return 0;
+}
