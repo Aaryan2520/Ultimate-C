f8a2fe2c7c42e14566e8ae648b2ba91c6cba4172
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-08-11 23:38:22 +0530

    Update 02_arithmetic_operators.c

--- CHAPTER 2/02_arithmetic_operators.c
+++ CHAPTER 2/02_arithmetic_operators.c
@@ -2,10 +2,14 @@
 
 int main()
 {
-    int a = 3;
-    int b = 4;
+    int a = 5;
+    int b = 3;
     int c = a + b;
-    printf("The value of a is %d and value of b is %d and sum is %d", a, b, c);
+    printf("The value of a is %d and value of b is %d and sum is %d \n", a, b, c);
+
+    // Modulus operator is used to get the remainder.
+    printf("The remainder when a is devided by b is : %d \n", a % b);
+    printf("%d",4^4);
 
     return 0;
 }
