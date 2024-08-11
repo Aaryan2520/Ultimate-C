d3a9d827282a984e7749aa01d580c73bcaac4e91
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-08-11 15:12:55 +0530

    Update 02_arithmetic_operators.c

--- CHAPTER 2/02_arithmetic_operators.c
+++ CHAPTER 2/02_arithmetic_operators.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    printf("Yesh Raj");
+int main()
+{
+    int a = 3;
+    int b = 4;
+    int c = a + b;
+    printf("The value of a is %d and value of b is %d and sum is %d", a, b, c);
 
     return 0;
 }
